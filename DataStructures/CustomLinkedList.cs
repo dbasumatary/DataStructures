f88f1b60e6d41d56214554fbf65d0d250dc9552f
dbasumatary <basudhondip@gmail.com>
2023-06-05 14:17:33 +0530

    Added: Program to add first to linked list

---
+++ DataStructures/CustomLinkedList.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DataStructures
+{
+    public class Node<T>
+    {
+        public T Data { get; set; }
+        public Node<T> Next { get; set; }
+
+        public Node(T data)
+        {
+            this.Data = data;
+        }
+    }
+    public class CustomLinkedList<T>
+    {
+        private Node<T> head;
+
+        //UC1-Add first method in linked list
+        public void AddFirst(Node<T> newNode)
+        {
+            if (head == null)
+            {
+                head = newNode;
+                return;
+            }
+
+            Node<T> previous = head;
+            head = newNode;
+            newNode.Next = previous;
+        }
+
+        public void Display()
+        {
+            Node<T> temp = head;
+            while (temp != null)
+            {
+                Console.Write(temp.Data + "->");
+                temp = temp.Next;
+            }
+            Console.WriteLine("END");
+        }
+    }
+}
--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Data structures implementation\n");
+            CustomLinkedList<int> list = new CustomLinkedList<int>();
+            Node<int> firstNode = new Node<int>(23);
+            Node<int> secondNode = new Node<int>(34);
+            Node<int> thirdNode = new Node<int>(29);
+            Node<int> fourthNode = new Node<int>(15);
+            Node<int> fifthNode = new Node<int>(42);
+
+            list.AddFirst(firstNode);
+            list.AddFirst(secondNode);
+            list.AddFirst(thirdNode);
+            list.AddFirst(fourthNode);
+            list.AddFirst(fifthNode);
+
+            list.Display();
         }
     }
 }
